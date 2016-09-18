111b5548f6841a3b0d0a268283a2e57cff6ae08d
darko3 <skrillex2408@gmail.com>
2016-09-18 13:06:15 -0300

    Added build_exe.py to build your own executable files from the source code with cx_Freeze

--- build_exe.py
+++ build_exe.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python2
 
+# Use this script if you need to build executable files for the program
+# This script works for Windows & Linux operating systems
+# * Make sure you have the cx_Freeze module installed for Python: http://cx-freeze.sourceforge.net
+# To build executables, run: "python build_exe.py build" in a terminal (or in CMD if you're on Windows)
+
 import os
 import sys
 import time
 import getopt
+from color import Color as tcolor
+from color import textType as ttype
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["os", "sys", "time", "getopt"]}
+build_exe_options = {"packages": ["os", "sys", "time", "getopt", "color"]}
 
-includes = ["sys", "os", "time", "getopt"]
+includes = ["sys", "os", "time", "getopt", "color"]
 
 setup(name='batch-renamer-cli', version='1.0.2b', description='Batch rename files in a folder quickly.', options = {"build_exe": {"includes":includes}}, executables= [Executable("batch-renamer-cli.py")])
